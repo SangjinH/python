d0df87b9c819368b1f360ac32c13905239ec001b
SangjinH <54434944+SangjinH@users.noreply.github.com>
2020-09-11 18:04:24 +0900

    이진탐색

---
+++ lecture_note/정렬_이진탐색/특정수의_개수.py
@@ -0,0 +1,19 @@
+# N개의 원소를 포함하는 수열이 오름차순으로 정렬되어 있을떄,
+# 수열에서 x가 등장하는 횟수를 구하면 ?
+
+from bisect import bisect_right,bisect_left
+
+def count_by_range(array, left_value, right_value):
+    right_index = bisect_right(array,right_value)
+    left_index = bisect_left(array, left_value)
+    return right_index - left_index
+
+n, m = map(int, input().split())
+array = list(map(int, input().split()))
+
+count = count_by_range(array, x, x)
+
+if count == 0:
+    print(-1)
+else:
+    print(count)

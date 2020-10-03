fd6548adbf78f7546406b1c7817be06dc8bf7a46
SangjinH <54434944+SangjinH@users.noreply.github.com>
2020-10-03 19:54:15 +0900

    백준 알고리즘, DP , 퇴사

--- algorithm/baekjoon/Dynamic_Programming/14501.py
+++ algorithm/baekjoon/Dynamic_Programming/14501.py
@@ -18,4 +18,16 @@ for i in range(n):
     day.append(arr[i][0])
     pay.append(arr[i][1])
 
+dp = [0] * (n+1)
+
+for i in range(n):
+    if i + day[i] <= n:
+        dp[i] = pay[i]
 print(day)
+print(dp)
+
+for i in range(n):
+    if i + day[i] <= n:
+        dp[i] = max(pay[i] + dp[i+day[i]], dp[i+1])
+
+print(dp)

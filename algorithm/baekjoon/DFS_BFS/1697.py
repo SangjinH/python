b620053c3df073cea13515c3b2b494781c66ab5a
SangjinH <54434944+SangjinH@users.noreply.github.com>
2020-10-06 14:23:45 +0900

    백준 알고리즘, BFS 숨바꼭질2

--- algorithm/baekjoon/DFS_BFS/1697.py
+++ algorithm/baekjoon/DFS_BFS/1697.py
@@ -1,31 +1,59 @@
-# https://www.acmicpc.net/problem/1697
-# 백준 1697번 , BFS, DFS 숨바꼭질
+# # https://www.acmicpc.net/problem/1697
+# # 백준 1697번 , BFS, DFS 숨바꼭질
+# import sys
+# input = sys.stdin.readline
+# from collections import deque
+#
+# n, k = map(int, input().rstrip().split())
+#
+#
+# total = []
+# range_list = [n]
+# total.append(range_list)
+# while True:
+#
+#     tmp = []
+#     for i in range_list:
+#         tmp.append(i-1)
+#         tmp.append(i+1)
+#         tmp.append(i*2)
+#     total.append(tmp)
+#     range_list = tmp
+#
+#     if k in tmp:
+#         break
+#
+#
+# if n == k:
+#     print(0)
+# else:
+#     print(len(total)-1)
+
 import sys
+
 input = sys.stdin.readline
 from collections import deque
 
 n, k = map(int, input().rstrip().split())
 
+d = [0] * 100001
 
-collects = []
-def bfs(start, target):
-    collects.append([start])
-    num = start
-    temp_list = [start]
-    while True:
-        li = []
-        for i in temp_list:
-            a = i-1
-            b = i+1
-            c = i*2
-            li.append(a)
-            li.append(b)
-            li.append(c)
+q = deque([n])
 
-        collects.append(li)
-        temp_list = li
-        if target in collects:
-            break
+while True:
+    if n == k:
+        break
 
-bfs(n,k)
-print(collects)
+    num = q.popleft()
+
+    if 0 <= num - 1 <= 100000 and d[num - 1] == 0:
+        d[num - 1] = d[num] + 1
+        q.append(num - 1)
+    if 0 <= num + 1 <= 100000 and d[num + 1] == 0:
+        d[num + 1] = d[num] + 1
+        q.append(num + 1)
+    if 0 <= num * 2 <= 100000 and d[num * 2] == 0:
+        d[num * 2] = d[num] + 1
+        q.append(num * 2)
+
+print(d[k])

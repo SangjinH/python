22815a6f34c25c84458e77335e496e7c36fa1262
SangjinH <54434944+SangjinH@users.noreply.github.com>
2020-10-08 11:31:51 +0900

    백준 알고리즘, 9465번 다익스트라 # 연결고리

--- algorithm/baekjoon/dijkstra/1753.py
+++ algorithm/baekjoon/dijkstra/1753.py
@@ -3,3 +3,41 @@
 
 import sys
 input = sys.stdin.readline
+import heapq
+INF = int(1e9)
+
+def dijkstra(start):
+    q = []
+    heapq.heappush(q,(0, start))
+    distance[start] = 0
+
+    while q:
+        dist, now = heapq.heappop(q)
+        if dist < distance[now]:
+            continue
+
+        for i in graph[now]:
+            cost = dist + i[1]
+
+            if cost < distance[i[0]]:
+                distance[i[0]] = cost
+                heapq.heappush(q, (cost, i[0]))
+
+
+v, e = map(int, input().rstrip().split())
+start = int(input().rstrip())
+
+graph = [[] for _ in range(v+1)]
+distance = [INF] * (v+1)
+
+for _ in range(e):
+    a, b, c = map(int, input().split())
+    graph[a].append((b, c))
+
+dijkstra(start)
+
+for i in range(1, len(distance)):
+    if distance[i] == INF:
+        print('INF')
+    else:
+        print(distance[i])

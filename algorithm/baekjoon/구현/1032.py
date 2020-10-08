94c7f69065f612a7474e48a77276cabe64dbf153
SangjinH <54434944+SangjinH@users.noreply.github.com>
2020-10-08 14:52:16 +0900

    백준 알고리즘, 9465번  DP 오르막길

---
+++ algorithm/baekjoon/구현/1032.py
@@ -0,0 +1,12 @@
+# https://www.acmicpc.net/problem/1032
+# 백준 1032번, 구현, 명령 프롬프트
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+n = int(input())
+
+
+texts = set()
+
+for i in range
